2d4179602779f04885556aea0bd0eb2ba2749543
icemanpolar <165238909+icemanpolar@users.noreply.github.com>
2024-03-27 21:51:07 -0600

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'contacto@hielolapolar.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -33,9 +33,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['Nombre'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['Mensaje'], 'Message', 10);
 
   echo $contact->send();
 ?>
